401542b3558b4ab5852a305d731a12030a03d3ea
fjrlfrq <fasyarabil21@gmail.com>
2022-12-19 21:13:04 +0700

    challenge21

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,25 @@
 var express = require('express');
+const User = require('../models/user')
 var router = express.Router();
+const moment = require('moment')
 
 /* GET users listing. */
 module.exports = function (db) {
   router.get('/', function (req, res, next) {
-    res.render('users/user');
+    User.read(db, function (users) {
+      res.render('users/user', { users, moment });
+    })
+  });
+
+  router.get('/add', function (req, res, next) {
+    res.render('users/add');
+  });
+
+  router.post('/add', function (req, res, next) {
+    const { string, integer, float, daten, boolean } = req.body
+    User.add(db, string, integer, float, daten, boolean, () => {
+      res.redirect('/users')
+    })
   });
 
   return router;
